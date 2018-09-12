d7da72e6cb38ded59a983490bec70f1d8760c145
linjiaqin <1263457581@qq.com>
2018-09-12 22:31:20 +0800

    需要在application中增加包含controller包的位置。

--- demo/src/main/java/com/example/controller/ControllerTwo.java
+++ demo/src/main/java/com/example/controller/ControllerTwo.java
@@ -1,15 +1,12 @@
 package com.example.controller;
 
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
+
 @RestController
-public class ControllerOne {
-	@RequestMapping("/h")
-	public String index() {
-	    return "Hello World";
-	}
- 
+public class ControllerTwo {
+	    @RequestMapping("/hello")
+	    public String index() {
+	        return "Hello World";
+	    }
 }
--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 /*
  * 这是程序的入口类
  */
 
 @SpringBootApplication
+@ComponentScan("com.example.controller")//包
 public class DemoApplication {
 
 	public static void main(String[] args) {
