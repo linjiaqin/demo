7741b9a3bede89a10cb011c76f4bc8dbda744273
linjiaqin <1263457581@qq.com>
2018-09-12 21:58:43 +0800

    增加了一个controller和对application加了注释

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * 这是程序的入口类
+ */
 
 @SpringBootApplication
 public class DemoApplication {
